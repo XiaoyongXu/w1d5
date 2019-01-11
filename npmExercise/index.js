6923c1b58c126682e9d33a17e6bec3faa15df497
XiaoyongXu <xxy1994212@gmail.com>
2019-01-11 19:14:10 +0000

    complete the npm intall and test of chalk

---
+++ npmExercise/index.js
@@ -0,0 +1,6 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.yellow("World") + chalk.red(" test") + chalk.blue(" success");
+
+
+console.log(message);
---
+++ npmdemo/index.js
@@ -0,0 +1,12 @@
+var cowsay = require("cowsay");
+var chalk = require('chalk');
+
+console.log(cowsay.say({
+	text : "I'm a moooodule",
+	e : "oO",
+	T : "U "
+}));
+
+
+ 
+console.log(chalk.blue('Hello world!'));
